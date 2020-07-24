277c629016326836ee979fc79f8bf8838c7c6272
KanishkKalyani <you@example.comkanishkkalyani@gmail.com>
2020-07-24 19:09:58 +0530

    Updates

---
+++ precache-manifest.c10145381e944fe1043e5edc6738d7e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fc922ecb9a3023c42b7bc993672a49e6",
+    "url": "/Notes-React-App/index.html"
+  },
+  {
+    "revision": "c6bde8dcbeb2976a9eb1",
+    "url": "/Notes-React-App/static/css/main.9dafee1a.chunk.css"
+  },
+  {
+    "revision": "99503f3c98ee37568da8",
+    "url": "/Notes-React-App/static/js/2.7027edf7.chunk.js"
+  },
+  {
+    "revision": "40158217dcba29c41bf6c1afe3c208d6",
+    "url": "/Notes-React-App/static/js/2.7027edf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6bde8dcbeb2976a9eb1",
+    "url": "/Notes-React-App/static/js/main.172ae33d.chunk.js"
+  },
+  {
+    "revision": "1a5477d19092d9a886bc",
+    "url": "/Notes-React-App/static/js/runtime-main.e5c7b9d1.js"
+  }
+]);
